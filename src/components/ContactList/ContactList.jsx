619a7a1ad28ff4aa9a73cccaaa707a16923b488d
Marianna Nishovych <nisovicmarianna@gmail.com>
2024-12-03 17:19:05 +0200

    add get requests

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import Contact from '../Contact/Contact';
 import css from './ContactList.module.css';
-import { deleteContact } from '../../redux/contactsSlice';
+import { deleteContact } from '../../redux/contactsOps';
 
 const ContactList = () => {
 
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import data from '../contacts.json'
+import { fetchContacts, deleteContact, addContact } from './contactsOps';
 
 const initialState = {
     items: data,
@@ -16,7 +17,22 @@ export const contactsSlice = createSlice({
         state.items = state.items.filter((item) => item.id !== action.payload);
     },
     },
+    extraReducers: (builder) => {
+        builder
+        .addCase(fetchContacts.fulfilled, (state, action) => {
+            state.items = action.payload;
+        })
+        .addCase(fetchContacts.pending, (state, action) => {
+            state.isLoading = true;
+        })
+        .addCase(deleteContact.fulfilled, (state, action) => {
+state.items = state.items.filter(item => item.id !== action.payload.id);
+        })
+        .addCase(addContact.fulfilled, (state, action) => {
+            state.items.push(action.payload);
+        })
+    }
 });
 
 export const contactsReducer = contactsSlice.reducer;
-export const { addContact, deleteContact } = contactsSlice.actions;
+// export const { addContact} = contactsSlice.actions;
