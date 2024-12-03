08bb38608a550fae4f855f81b1aa8f1c31a363bc
Marianna Nishovych <nisovicmarianna@gmail.com>
2024-12-03 18:40:25 +0200

    add metcher

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import './App.css';
 import ContactList from './components/ContactList/ContactList';
 import SearchBox from './components/SearchBox/SearchBox';
 import ContactForm from './components/ContactForm/ContactForm';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchContacts } from './redux/contactsOps';
+import { selectIsError, selectIsLoading } from './redux/contactsSlice'
 
 function App() {
+
+  const isLoading = useSelector(selectIsLoading);
+  const isError = useSelector(selectIsError);
 
   const dispatch = useDispatch();
 
@@ -19,6 +23,8 @@ function App() {
       <h1>Phonebook</h1>
       <ContactForm />
       <SearchBox />
+      {isLoading && <p>Loading...</p>}
+      {isError && <p>{isError}</p>}
       <ContactList />
     </div>
   );
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
 import data from '../contacts.json'
 import { fetchContacts, deleteContact, addContact } from './contactsOps';
 
@@ -22,17 +22,28 @@ export const contactsSlice = createSlice({
         .addCase(fetchContacts.fulfilled, (state, action) => {
             state.items = action.payload;
         })
-        .addCase(fetchContacts.pending, (state, action) => {
-            state.isLoading = true;
-        })
         .addCase(deleteContact.fulfilled, (state, action) => {
 state.items = state.items.filter(item => item.id !== action.payload.id);
         })
         .addCase(addContact.fulfilled, (state, action) => {
             state.items.push(action.payload);
         })
+        .addMatcher(isAnyOf(fetchContacts.pending, deleteContact.pending, addContact.pending), (state) => {
+            state.isError = false;
+            state.isLoading = true;
+        })
+        .addMatcher(isAnyOf(fetchContacts.rejected, deleteContact.rejected, addContact.rejected), (state, action) => {
+            state.isError = action.payload;
+            state.isLoading = false;
+        })
+        .addMatcher(isAnyOf(fetchContacts.fulfilled, deleteContact.fulfilled, addContact.fulfilled), (state, action) => {
+            state.isLoading = false;
+        })
     }
 });
 
+
+export const selectIsLoading = state => state.contacts.isLoading;
+export const selectIsError = state => state.contacts.isError;
 export const contactsReducer = contactsSlice.reducer;
 // export const { addContact} = contactsSlice.actions;
